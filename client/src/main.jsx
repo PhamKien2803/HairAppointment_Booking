1ba7e41d5f85722fc8f8d05104e0b20ae915c4fc
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-05-18 23:49:47 +0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-axios.defaults.baseURL = "http://localhost:9999";
+axios.defaults.baseURL = "http://localhost:7071/api/";
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
