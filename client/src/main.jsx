71594a8cd99f70af0bbe0f578270b1d1abd924f8
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-05-20 09:35:42 +0700

    Again deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 axios.defaults.baseURL = "http://localhost:7071/api/";
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
     <ToastContainer />
-  </Provider>
+  </React.StrictMode>
 )
