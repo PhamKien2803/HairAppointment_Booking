34aa4c7928684556d86f03e6c59b9f30a0da410f
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-05-20 13:42:18 +0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-axios.defaults.baseURL = "http://localhost:9999";
+// axios.defaults.baseURL = "http://localhost:7071/api/";
+// axios.defaults.baseURL = "https://icy-hill-02852260f.6.azurestaticapps.net/api/"; URL của Azure Static Web Apps
+axios.defaults.baseURL = "https://your-api-functions-app.azurewebsites.net/api/"; // URL của Azure Functions App
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
